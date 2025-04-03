496c4c1cf6a9d2716cf9489b54e2e7da95905e4c
Alan-AC <136497721+Alan-AC@users.noreply.github.com>
2025-04-03 10:31:00 +0200

    goBack garden

--- garden.js
+++ garden.js
@@ -1,13 +1,19 @@
 function toggleCategory(categoryId) {
     var categoryDiv = document.querySelector('.categories');
-    var categories = document.querySelectorAll('.category');
-    categories.forEach(function(category) {
-        category.style.display = 'none';
+    if(categoryDiv){
         categoryDiv.style.display = 'none';
-    });
+    }
 
     var category = document.getElementById(categoryId);
     if (category) {
         category.style.display = 'block';
     }
+}
+
+function goBack(categoryId) {
+    var categoryDiv = document.querySelector('.categories');
+    var category = document.getElementById(categoryId);
+    categoryDiv.style.display = 'block';
+    category.style.display = 'none';
+    
 }
