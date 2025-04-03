4a72722dd7d32de8e89b500f0ccc1e3b2251eb20
Alan-AC <136497721+Alan-AC@users.noreply.github.com>
2025-04-03 10:05:27 +0200

    error js

--- garden.js
+++ garden.js
@@ -1,6 +1,5 @@
 function toggleCategory(categoryId) {
     var categoryDiv = document.querySelector('.categories');
-    var areasDiv = document.querySelector('.areas');
     var categories = document.querySelectorAll('.category');
     categories.forEach(function(category) {
         category.style.display = 'none';
@@ -9,22 +8,6 @@ function toggleCategory(categoryId) {
 
     var category = document.getElementById(categoryId);
     if (category) {
-        areasDiv.style.display = 'block';
         category.style.display = 'block';
-    }
-}
-
-function toggleArea(categoryId) {
-    var areasDiv = document.querySelector('.areas');
-    var plants = document.querySelectorAll('.plants');
-    plants.forEach(function(plant) {
-        plant.style.display = 'none';
-        areasDiv.style.display = 'none';
-    });
-
-    var plant = document.getElementById(categoryId);
-    if (plant) {
-        areasDiv.style.display = 'block';
-        plant.style.display = 'block';
     }
 }
