d5bf2d442c439f0b973f86865c68f92a5186851d
Alan-AC <136497721+Alan-AC@users.noreply.github.com>
2025-04-03 11:14:51 +0200

    mostrar y ocultar datos

--- garden.js
+++ garden.js
@@ -15,5 +15,25 @@ function goBack(categoryId) {
     var category = document.getElementById(categoryId);
     categoryDiv.style.display = 'block';
     category.style.display = 'none';
-    
+}
+
+function showData(pId) {
+    var plantsDiv = document.querySelector('.plants');
+    plantsDiv.style.display = 'none';
+
+    var divData = document.getElementById('divData');
+    divData.style.display = 'block';
+    var plantName = document.getElementById(pId).textContent;
+    document.getElementById('selectedPlant').innerHTML = '<p>' + plantName + '</p>';
+    document.getElementById('ext-int').innerHTML = '<p>' + pId + '</p>';
+}
+
+function goBack1() {
+    var ext_int = document.getElementById('ext-int').innerHTML;
+    document.getElementById('divData').style.display = 'none';
+    if(ext_int.includes('ext')){
+        document.getElementById('ext').style.display = 'block';
+    } else {
+        document.getElementById('int').style.display = 'block';
+    }
 }
